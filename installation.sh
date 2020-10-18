03061e4fc8266308713981acd633e3ee1af06883
Izaird Mothelet <iza_alex3@hotmail.com>
2020-10-18 00:28:03 -0500

    Added git config

---
+++ installation.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# INSTALL SCRIPT FOR MANJARO	
+# Installing some basic system utilities first.
+
+cd ~
+
+#sudo pacman -Syu
+#sudo pacman -S yay ranger telegram-desktop python-pip\
+#		chromium --noconfirm
+
+
+#sudo pip install powerline-shell
+
+
+# Git configuration
+git config --global user.email "iza_alex3@hotmail.com"
+git config --global user.name "Izaird Mothelet"
+
+echo "Listo"
