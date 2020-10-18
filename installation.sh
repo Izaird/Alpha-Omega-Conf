adbf1918f30d5ed9198fe8775c1f49375dd5345c
Izaird Mothelet <iza_alex3@hotmail.com>
2020-10-18 02:02:15 -0500

    Separacion de configuracion de zsh

--- installation.sh
+++ installation.sh
@@ -35,10 +35,3 @@ echo "Moving config files"
 #change of terminal
 zsh /usr/share/zsh/functions/Newuser/zsh-newuser-install -f
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-sudo dnf install fd-find
-chsh -s $(which zsh)
