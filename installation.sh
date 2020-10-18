94dbc052c67c8f29410b2387fd18d4ec2d6ca31d
Izaird Mothelet <iza_alex3@hotmail.com>
2020-10-18 02:52:50 -0500

    added new config file

--- installation.sh
+++ installation.sh
@@ -6,29 +6,29 @@
 cd ~
 
 echo "Installation with pacman"
-# sudo pacman -Syu
-# sudo pacman -S yay ranger telegram-desktop python-pip chromium \
-#                code zsh --noconfirm
+sudo pacman -Syu
+sudo pacman -S yay ranger telegram-desktop python-pip chromium \
+               code zsh --noconfirm
 
 echo "Installation with yay"
-# yay -S snapd  --noconfirm
+yay -S snapd  --noconfirm
 
 echo "Installation with snapd"
-# sudo systemctl enable --now snapd.socket
-# sudo snap install spotify
+sudo systemctl enable --now snapd.socket
+sudo snap install spotify
 
 echo "Installation with pip"
 sudo pip install powerline-shell
 
 echo "Creation of directorys"
-# mkdir Code
-# mkdir -p ~/.config/powerline-shell && \
-# powerline-shell --generate-config > ~/.config/powerline-shell/config.json
+mkdir Code
+mkdir -p ~/.config/powerline-shell && \
+powerline-shell --generate-config > ~/.config/powerline-shell/config.json
 
 echo "Moving config files"
-# sudo cp Alpha-Omega-Conf/config-files/.gitconfig ~/.gitconfig
-# sudo cp Alpha-Omega-Conf/config-files/.bashrc ~/.bashrc
-# sudo cp Alpha-Omega-Conf/config-files/powerline.json ~/.config/powerline-shell/config.json
+sudo cp Alpha-Omega-Conf/config-files/.gitconfig ~/.gitconfig
+sudo cp Alpha-Omega-Conf/config-files/.bashrc ~/.bashrc
+sudo cp Alpha-Omega-Conf/config-files/powerline.json ~/.config/powerline-shell/config.json
 sudo cp Alpha-Omega-Conf/config-files/xorg.conf /etc/X11/xorg.conf
 
 #change of terminal
